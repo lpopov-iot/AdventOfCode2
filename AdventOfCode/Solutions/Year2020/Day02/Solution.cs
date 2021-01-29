2a4e967919478cf6c18df23d8f097a31b144927f
Lyud Popov <lyud96@hotmail.co.uk>
2021-01-29 18:20:11 +0000

    Gitignore and common ctor.

--- AdventOfCode/Solutions/Year2020/Day02/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day02/Solution.cs
@@ -7,15 +7,40 @@ namespace AdventOfCode.Solutions.Year2020
 
     class Day02 : ASolution
     {
-
+        string[] _input;
+        
         public Day02() : base(02, 2020, "")
         {
-
+            _input = Input.Split("\n");
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            int returnSum = 0;
+            
+            foreach (var line in _input)
+            {
+                var policy = ExtractPolicy(line);
+            }
+            return returnSum.ToString();
+        }
+
+        record Challenge
+        {
+            public Policy Policy;
+            public string Password { get; init; }
+        }
+        
+        record Policy
+        {
+            public char? ChallengeChar { get; init; }
+            public string LowerBound { get; init; }
+            public string UpperBound { get; init; }
+        }
+
+        private Challenge ExtractPolicy(string line)
+        {
+            return new Challenge();
         }
 
         protected override string SolvePartTwo()

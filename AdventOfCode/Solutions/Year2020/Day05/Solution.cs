cf107389182ffdfdb43353992dc009a86fad59d3
Lyud Popov <lyud96@hotmail.co.uk>
2021-02-07 14:49:02 +0000

    Day 5 p2.

--- AdventOfCode/Solutions/Year2020/Day05/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day05/Solution.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AdventOfCode.Solutions.Year2020
@@ -24,11 +25,50 @@ namespace AdventOfCode.Solutions.Year2020
                 var row = BSPFind(0, 127, instruction.Substring(0, instruction.Length - 3).ToCharArray());
                 var column = BSPFind(0, 7, instruction.Substring(instruction.Length - 3).ToCharArray());
 
-                var seatId = row * 8 + column;
+                var seatId = GetSeatId(row, column);
                 curMaxSeatId = seatId > curMaxSeatId ? seatId : curMaxSeatId;
             }
 
             return curMaxSeatId.ToString();
+        }
+
+        protected override string SolvePartTwo()
+
+        {
+            var seats = new List<Tuple<int, int>>();
+            foreach (var instruction in _input)
+            {
+                var row = BSPFind(0, 127, instruction.Substring(0, instruction.Length - 3).ToCharArray());
+                var column = BSPFind(0, 7, instruction.Substring(instruction.Length - 3).ToCharArray());
+
+                seats.Add(new Tuple<int, int>(row, column));
+            }
+
+            var orderedSeats = seats.OrderBy(tuple => tuple.Item1)
+                .ThenBy(tuple => tuple.Item2).ToList();
+
+            for (var i = 0; i < 127; i++)
+            {
+                var row = orderedSeats.Where(x => x.Item1 == i).ToList();
+
+                if (row.Any())
+                {
+                    for (var x = 0; x < row.Count - 1; x++)
+                    {
+                        if (row[x + 1].Item2 - row[x].Item2 == 2)
+                        {
+                            return GetSeatId(row[x].Item1, row[x].Item2 + 1).ToString();
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private int GetSeatId(int row, int column)
+        {
+            return  row * 8 + column;
         }
 
         private int BSPFind(int min, int max, char[] instruction)
@@ -49,11 +89,6 @@ namespace AdventOfCode.Solutions.Year2020
             }
 
             return mid + 1;
-        }
-
-        protected override string SolvePartTwo()
-        {
-            return null;
         }
     }
 }

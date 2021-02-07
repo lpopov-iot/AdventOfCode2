4c69c76f1b75e8b1525c9b09114122aa408661d5
Lyud Popov <lyud96@hotmail.co.uk>
2021-02-07 12:50:48 +0000

    Day 5 p1.

--- AdventOfCode/Solutions/Year2020/Day05/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day05/Solution.cs
@@ -8,14 +8,47 @@ namespace AdventOfCode.Solutions.Year2020
     class Day05 : ASolution
     {
 
+        string[] _input;
+
         public Day05() : base(05, 2020, "")
         {
-
+            _input = Input.Split("\n");
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            var curMaxSeatId = 0;
+
+            foreach (var instruction in _input)
+            {
+                var row = BSPFind(0, 127, instruction.Substring(0, instruction.Length - 3).ToCharArray());
+                var column = BSPFind(0, 7, instruction.Substring(instruction.Length - 3).ToCharArray());
+
+                var seatId = row * 8 + column;
+                curMaxSeatId = seatId > curMaxSeatId ? seatId : curMaxSeatId;
+            }
+
+            return curMaxSeatId.ToString();
+        }
+
+        private int BSPFind(int min, int max, char[] instruction)
+        {
+            var mid = (min + max + 1) / 2;
+
+            for (var i = 0; i < instruction.Length; i++)
+            {
+                if (instruction[i] == 'F' || instruction[i] == 'L')
+                {
+                    return BSPFind(min, mid, instruction[(i+1)..]);
+                }
+
+                if (instruction[i] == 'B' || instruction[i] == 'R')
+                {
+                    return BSPFind(mid, max, instruction[(i+1)..]);
+                }
+            }
+
+            return mid - 1;
         }
 
         protected override string SolvePartTwo()
